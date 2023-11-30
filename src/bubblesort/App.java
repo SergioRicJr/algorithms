af4c79c3f7bfef0c99a6932c366ac6d8bed80cbd
Sergio <sergioricjr7@gmail.com>
2023-11-29 23:49:36 -0300

    refactor bubblesort

--- src/bubblesort/App.java
+++ src/bubblesort/App.java
@@ -5,12 +5,23 @@ public class App {
     public static int[] bubbleSort(int[] array) {
         int arraySize = array.length;
         int temp = 0;
-        for (int x = 0; x < arraySize; x++) {
-            for (int y = 1; y < (arraySize-x); y++) {
-                if(array[y-1] > array[y]){  
-                    temp = array[y-1];  
-                    array[y-1] = array[y];  
-                    array[y] = temp;  
+        // for (int x = 0; x < arraySize; x++) {
+        //     for (int y = 1; y < (arraySize-x); y++) {
+        //         if(array[y-1] > array[y]){  
+        //             temp = array[y-1];  
+        //             array[y-1] = array[y];  
+        //             array[y] = temp;  
+        //         }  
+        //     }
+            
+        // }
+        
+        for (int x = 0; x < (arraySize - 1); x++) {
+            for (int y = 0; y < (arraySize - 1); y++) {
+                if(array[y] > array[y+1]){  
+                    temp = array[y];  
+                    array[y] = array[y+1];  
+                    array[y+1] = temp;  
                 }  
             }
             
